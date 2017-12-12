af03a6330c53ce20c250415a25365b26e02e1c3f
jiayang <jiayang1992>
2017-12-12 16:03:13 +0800

    2017/11/17

---
+++ 镇川镇综合服务平台/thread1_ZCZ_all_testcases/case_验证农户管理_农户信息_按查看进行查询.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re,os
+
+class Case(unittest.TestCase):
+    def setUp(self):
+        option = webdriver.ChromeOptions()
+        executable_path = "C:\\Users\\admin\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe"
+        os.environ['webdriver.chrome.driver'] = executable_path
+        self.driver = webdriver.Chrome(executable_path, chrome_options=option)
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://101.201.41.228:7006/ylsn1.5/manage/user/home"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_case(self):
+        driver = self.driver
+        driver.get(self.base_url + "/ylsn1.5/manage/user/home")
+        driver.maximize_window()
+        driver.find_element_by_id("userNameCopy").clear()
+        driver.find_element_by_id("userNameCopy").send_keys("admin")
+        driver.find_element_by_id("userPassword").clear()
+        driver.find_element_by_id("userPassword").send_keys("666666")
+        driver.find_element_by_id("btnLogin").click()
+        driver.find_element_by_css_selector("li.unnum1").click()
+        driver.find_element_by_id("menu-sm0").click()
+        driver.switch_to_frame(driver.find_element_by_class_name("iframeClass"))
+        Select(driver.find_element_by_id("villageId")).select_by_visible_text(u"八塌湾村委会")
+        Select(driver.find_element_by_id("houseReId")).select_by_visible_text(u"配偶")
+        driver.find_element_by_id("queryBtn").click()
+        # ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
+        driver.find_element_by_id("310051696055").click()
+        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
+        driver.switch_to_frame(driver.find_element_by_id("atrDialogIframe_userId"))
+        driver.find_element_by_id("switch-big2").click()
+        driver.find_element_by_id("310051696055").click()
+        driver.find_element_by_id("310051696056").click()
+        driver.find_element_by_id("switch-big3").click()
+        driver.find_element_by_id("switch-big4").click()
+        driver.find_element_by_xpath(u"//input[@value='下一户']").click()
+        self.assertEqual(u"网络异常异常，请检查网络连接！", self.close_alert_and_get_its_text())
+        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
+        driver.switch_to_default_content()
+        driver.switch_to_frame(driver.find_element_by_class_name("iframeClass"))
+        driver.find_element_by_id("closeId").click()
+        time.sleep(5)
+        driver.find_element_by_id("310051793287").click()
+        driver.switch_to_frame(driver.find_element_by_id("atrDialogIframe_userId"))
+        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
+        driver.find_element_by_id("switch-big2").click()
+        driver.find_element_by_id("switch-big3").click()
+        driver.find_element_by_id("switch-big4").click()
+        driver.find_element_by_xpath(u"//input[@value='下一户']").click()
+        self.assertEqual(u"网络异常异常，请检查网络连接！", self.close_alert_and_get_its_text())
+        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
+        driver.switch_to_default_content()
+        driver.switch_to_frame(driver.find_element_by_class_name("iframeClass"))
+        driver.find_element_by_id("closeId").click()
+        driver.switch_to_default_content()
+        driver.find_element_by_link_text(u"退出").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
